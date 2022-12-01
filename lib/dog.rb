d35249a239dcb64fdaaadf95ae93e151cbdbbce0
Mohammed Taitai <mohammedtaitai@outlook.com>
2022-12-01 14:43:55 -0500

    commit tree d97b038150fe5ff28b2993f86d9dbc65b669832b

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
+    def name=(val)
+        @name = val
+    end
 
+    def name
+        @name
+    end
+    def breed=(valB)
+        @breed = valB
+    end
+
+    def breed
+        @breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
 class Person
+    def name=(val)
+        @name = val
+    end
 
+    def name
+        @name
+    end
+    def job=(valJ)
+        @job = valJ
+    end
+
+    def job
+        @job
+    end
 end
